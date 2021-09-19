ee18cc788b7a4740229b3d90a6aa3b902b4c502b
Mohammad Eusha <eusha37@gmail.com>
2021-09-19 16:59:24 +0600

    Finished

--- src/Components/HomePage/Home/Home.js
+++ src/Components/HomePage/Home/Home.js
@@ -1,3 +1,5 @@
+import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import About from '../About/About';
 import Header from '../Header/Header';
@@ -9,7 +11,11 @@ const Home = () => {
         <div>
             <div className="bg-img d-flex align-items-center ">
                 <Navbar />
-                <Header />
+                <div className="d-flex align-items-center container">
+                    <FontAwesomeIcon icon={faArrowLeft} className="fa-4x pe-3" />
+                    <Header />
+                    <FontAwesomeIcon icon={faArrowRight} className="fa-4x ps-3" />
+                </div>
             </div>
             <div>
                 <About />
